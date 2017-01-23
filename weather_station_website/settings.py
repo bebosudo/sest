3bed03dcc11345e79584828fb68d0fb457bafbe3
bebo_sudo <bebo.sudo@gmail.com>
2017-01-23 03:46:07 +0100

    Remove pyc files & others.

--- weather_station_website/settings.py
+++ weather_station_website/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-from SECRET import SECRET_KEY
+from .secret_things import SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
